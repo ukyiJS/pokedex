11dd26804542719fdfc50ef19018a38a17da0925
UkYiJS <ukyi.js@gmail.com>
2020-12-15 01:31:32 +0900

    feat: navigationBar 추가

---
+++ src/views/layout/NavigationBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-header elevated>
+    <q-toolbar class="bg-grey-3 text-dark">
+      <q-img :src="require('img/main-logo.png')" style="width: 5rem" basic />
+      <q-toolbar-title>Pokedex</q-toolbar-title>
+      <q-btn flat round dense icon="whatshot" />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
