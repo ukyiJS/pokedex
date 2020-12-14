fea09c916d708847d264354b04ad057ba5d14805
UkYiJS <ukyi.js@gmail.com>
2020-12-15 01:16:22 +0900

    feat: apollo config 추가

--- src/config/apollo/index.js
+++ src/config/apollo/index.js
@@ -1,6 +1,6 @@
-const { InMemoryCache } = require('apollo-cache-inmemory');
-const { default: ApolloClient } = require('apollo-client');
-const { createHttpLink } = require('apollo-link-http');
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
 
 const httpLink = createHttpLink({ uri: 'https://gzhm34nn6g.execute-api.ap-northeast-2.amazonaws.com/api/graphql' });
 const cache = new InMemoryCache();
