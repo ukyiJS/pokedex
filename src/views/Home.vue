adb15e3902c6df5b458c8a7f8f7dd8e431d18bfc
UkYiJS <ukyi.js@gmail.com>
2020-12-15 01:31:50 +0900

    feat: autoComplete 추가

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  <q-page-container>
+    <q-page padding>
+      <auto-complete />
+    </q-page>
+    <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
+      <q-btn fab icon="keyboard_arrow_up" color="dark" />
+    </q-page-scroller>
+  </q-page-container>
 </template>
 
 <script>
+import AutoComplete from '@/components/AutoComplete.vue';
+
 export default {
+  components: { AutoComplete },
   name: 'Home',
 };
 </script>
