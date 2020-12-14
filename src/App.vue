862697d29520cecc2fb9468e997921bd3ee08b41
UkYiJS <ukyi.js@gmail.com>
2020-12-15 01:18:08 +0900

    feat: autoComplete 추가

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <q-header elevated>
       <q-toolbar class="bg-grey-3 text-dark">
         <q-img :src="require('img/main-logo.png')" style="width: 5rem" basic />
-
         <q-toolbar-title>Pokedex</q-toolbar-title>
         <q-btn flat round dense icon="whatshot" />
       </q-toolbar>
@@ -11,35 +10,30 @@
 
     <q-page-container>
       <q-page padding>
-        <div class="q-col-gutter-md row justify-center">
-          <div class="col-md-2 col-sm-4 col-xs-6">
-            <q-card>
-              <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" :ratio="1" basic spinner-color="white" />
-            </q-card>
-          </div>
-          <div class="col-md-2 col-sm-4 col-xs-6">
-            <q-card flat>
-              <q-skeleton height="150px" square />
-              <q-card-section>
-                <q-skeleton type="text" class="text-subtitle1" />
-                <q-skeleton type="text" width="50%" class="text-subtitle1" />
-                <q-skeleton type="text" class="text-caption" />
-              </q-card-section>
-            </q-card>
-          </div>
-        </div>
+        <auto-complete />
       </q-page>
-
       <q-page-scroller position="bottom-right" :scroll-offset="150" :offset="[18, 18]">
-        <q-btn fab icon="keyboard_arrow_up" color="accent" />
+        <q-btn fab icon="keyboard_arrow_up" color="dark" />
       </q-page-scroller>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
+import AutoComplete from './components/AutoComplete.vue';
+
 export default {
   name: 'LayoutDefault',
+  meta: {
+    title: 'Pokedex',
+    titleTemplate: title => `${title} - ukyi`,
+    meta: {
+      description: { name: '포켓몬도감', content: 'Page 1' },
+      keywords: { name: '포켓몬도감', content: 'Pokedex website' },
+      equiv: { 'http-equiv': 'Content-Type', content: 'text/html; charset=UTF-8' },
+    },
+  },
+  components: { AutoComplete },
 };
 </script>
 
